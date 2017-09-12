7d30c27bddf14516e9f804fac330beb471361396
yijiannijuxiao <1782946883@qq.com>
2017-09-12 11:34:01 +0800

    第二次

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.myapplication3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout activity_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        activity_main = (RelativeLayout) findViewById(R.id.activity_main);
     }
 }
